11309e2e98b01d986c4047e1afabf4cf405cfe8c
zhang <375942753@qq.com>
2019-06-22 14:32:28 +0800

    后台登录验证系统完成

--- adminIndex/views.py
+++ adminIndex/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from . import is_login
 
-
 # Create your views here.
+"""
+后台视图调用之前需要先判断用户是否登录，见show_index方法的示例。
+is_login方法是用来判断用户是否已经登录的方法
+"""
 
 
 def show_index(request):
