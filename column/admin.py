c89bd1cbdce4875ce8fd418254ee33ec44759793
zhang <375942753@qq.com>
2019-07-03 15:50:06 +0800

    分类数据结构更新

--- column/admin.py
+++ column/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from . import models
-# Register your models here.
 
+
+# Register your models here.
+class ColumnManager(admin.ModelAdmin):
+    li = ['columnId', 'parent', 'time', 'name']
+    list_display = li
+    list_display_links = li
+
+
+admin.site.register(models.Columns, ColumnManager)
